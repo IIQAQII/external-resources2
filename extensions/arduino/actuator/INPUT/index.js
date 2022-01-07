32f925f9a7e84fb6e716a48e6777f159b83d4024
白夜 <279262953@qq.com>
2022-01-07 02:30:02 +0000

    1

---
+++ extensions/arduino/actuator/INPUT/index.js
@@ -0,0 +1,26 @@
+const input = formatMessage => ({
+    name: formatMessage({
+        id: 'INPUT.name',
+        default: 'INPUT'
+    }),
+    extensionId: 'input',
+    version: '1.0.0',
+    supportDevice: ['arduinoUno', 'arduinoNano', 'arduinoMini', 'arduinoLeonardo',
+        'arduinoMega2560', 'arduinoEsp8266'],
+    author: 'Liu',
+    iconURL: `asset/INPUT.jpg`,
+    description: formatMessage({
+        id: 'INPUT.description',
+        default: 'INPUT drive module.'
+    }),
+    featured: true,
+    blocks: 'blocks.js',
+    generator: 'generator.js',
+    toolbox: 'toolbox.js',
+    msg: 'msg.js',
+    library: 'lib',
+    tags: ['actuator'],
+    helpLink: ''
+});
+
+module.exports = input;
